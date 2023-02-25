4fb2d9781ade66b8c8b6e863d55d275fc6291c65
Soham Govande <vgsoham@gmail.com>
2023-02-25 13:52:17 -0800

    landing page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import { Button, ChakraProvider, extendBaseTheme, extendTheme } from "@chakra-ui/react"
+import "@fontsource/inter/400.css"
+import "@fontsource/inter/900.css"
+import "@fontsource/inter/700.css"
+import "@/styles/globals.css"
+
+const theme = extendTheme({
+  fonts: {
+    heading: '"Inter", sans-serif',
+    body: '"Inter", sans-serif',
+  },
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
